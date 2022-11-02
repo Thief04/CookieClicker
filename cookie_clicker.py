47aa02fbee95c2fe2a8f2722de53be6ff129ed10
DeathTank1 <78882668+DeathTank1@users.noreply.github.com>
2022-11-02 19:26:29 -0400

    Cookie on Screen

--- cookie_clicker.py
+++ cookie_clicker.py
@@ -4,6 +4,12 @@ wn = turtle.Screen()
 wn.title("Cookie Clicker")
 wn.bgcolor("black")
 
+wn.register_shape("cookie.gif")
+
+cookie = turtle.Turtle()
+cookie.shape("cookie.gif")
+cookie.speed(0)
+
 
 
 
